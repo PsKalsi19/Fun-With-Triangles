4de136137cc77ae3a09c17048975668a8f108b6b
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2022-10-16 12:16:33 +0530

    fix: added two more questions after portfolio review in quiz.
    fix: added two more questions after portfolio review in quiz.

--- quiz.js
+++ quiz.js
@@ -3,16 +3,16 @@ const outputContainer = document.querySelector('.output-container');
 const error = document.querySelector('#error');
 const submit = document.querySelector('#submit-button');
 const quizForm = document.querySelector('#triangle-quiz-form')
-const solutions = ['90°', 'Right', 'Equilateral', 'Longest side of triangle']
+const solutions = ['90°', 'Right', 'Equilateral', 'Longest side of triangle', 'scalene triangle', 'isosceles triangle']
 
 submit.addEventListener('click', getFormData)
 
 function getFormData() {
     let quizData = new FormData(quizForm);
     let count = 0
-    let index=0
+    let index = 0
     for (let value of quizData.entries()) {
-        let questionNumber=(+value[0])-1
+        let questionNumber = (+value[0]) - 1
         if (value[1] === solutions[questionNumber]) {
             count = count + 1;
         }
@@ -21,7 +21,9 @@ function getFormData() {
     setOutput(`Your total score is ${count}`)
     if (index < solutions.length) {
         const remainingQuestion = solutions.length - index
-        setError(`${remainingQuestion} questions are remaining to be answered.`)
+        remainingQuestion === 1 ?
+            setError(`${remainingQuestion} question is remaining to be answered.`) :
+            setError(`${remainingQuestion} questions are remaining to be answered.`)
     }
     else {
         hideError();
